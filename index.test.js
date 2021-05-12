79e91e3faa375812af129c6f47f8a7e8ae392732
Cao Mai <caomai@rocketmail.com>
2021-05-11 23:19:34 -0700

    Add more unit tests

--- index.test.js
+++ index.test.js
@@ -2,8 +2,20 @@ const Convert = require('./index')
 const assert = require('assert').strict;
 
 
-describe("integration test", function() {
+describe("unit test", function() {
     it("should convet a single digit number", function() {
         assert.equal(Convert.convertNum(2), "two");
+    });
+});
+
+describe("unit test", function() {
+    it("should convet a double digit number", function() {
+        assert.equal(Convert.convertNum(23), "twenty three");
+    });
+});
+
+describe("unit test", function() {
+    it("should convet a triple digit number", function() {
+        assert.equal(Convert.convertNum(345), "three hundred and fourty five");
     });
 });
