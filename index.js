578a26e807e7400e8b89abd2a3e70dfbafe7d37c
Cao Mai <caomai@rocketmail.com>
2021-05-11 22:55:56 -0700

    Add function to convert nnumber of millions

--- index.js
+++ index.js
@@ -54,61 +54,92 @@ function doubleDigits(num) {
     if (strNum[1] == 0) {
       return byTens[num];
     } else {
-      return byTens[strNum[0] + "0"]  + " " + digits[strNum[1]];
+      return byTens[strNum[0] + "0"] + " " + digits[strNum[1]];
     }
   }
 }
 
 function tripleDigits(num) {
-    let strNum = num.toString()
-    let hundredsDigit = strNum[0]
+  let strNum = num.toString();
+  let hundredsDigit = strNum[0];
 
-    return digits[hundredsDigit] +  " " + exponents["100"] + " and " + doubleDigits(strNum.slice(1))
+  return (
+    digits[hundredsDigit] +
+    " " +
+    exponents["100"] +
+    " and " +
+    doubleDigits(strNum.slice(1))
+  );
 }
 
-function fourDigits(num) {
-    let strNum = num.toString()
-    let thousandsDigit = strNum.slice(0, strNum.length-3)
+function thousandsConvert(num) {
+  let strNum = num.toString();
+  let thousandsDigit = strNum.slice(0, strNum.length - 3);
 
-    var thousandsWord = ""
-    switch (thousandsDigit.length) {
-      case 1:
-        thousandsWord = singleDigit(thousandsDigit)
-        break
-      case 2:
-        thousandsWord = doubleDigits(thousandsDigit)
-        break
-      case 3:
-        thousandsWord = tripleDigits(thousandsDigit)
-        break
-    }
-
-    return thousandsWord + " " + exponents[1000] + " " + tripleDigits(strNum.slice(1))
-}
-
-
-function convertNum(num) {
-  let digitCount = num.toString().length
-  
-  switch (true) {
-    case digitCount == 1:
-      return singleDigit(num)
-    case digitCount == 2: 
-      return doubleDigits(num)
-    case digitCount == 3:
-      return tripleDigits(num)
-    case digitCount < 7:  
-      return fourDigits(num)
-    case digitCount < 12:
-      break
+  var thousandsWord = "";
+  switch (thousandsDigit.length) {
+    case 1:
+      thousandsWord = singleDigit(thousandsDigit);
+      break;
+    case 2:
+      thousandsWord = doubleDigits(thousandsDigit);
+      break;
+    case 3:
+      thousandsWord = tripleDigits(thousandsDigit);
+      break;
   }
 
+  return (
+    thousandsWord + " " + exponents[1000] + " " + tripleDigits(strNum.slice(1))
+  );
+}
+
+function millionConvert(num) {
+  let strNum = num.toString();
+  let millionDigit = strNum.slice(0, strNum.length - 6);
+
+  var millionWord = "" 
+  switch (millionDigit.length) {
+    case 1:
+      millionWord = singleDigit(millionDigit);
+      break;
+    case 2:
+      millionWord = doubleDigits(millionDigit);
+      break;
+    case 3:
+      millionWord = tripleDigits(millionDigit);
+      break;
+  }
+
+  let thousandSlice = strNum.slice(millionDigit.length)
+
+  return (
+    millionWord + " " + exponents[1000000] + " " + thousandsConvert(thousandSlice)
+  );
+
+}
+
+function convertNum(num) {
+  let digitCount = num.toString().length;
+
+  switch (true) {
+    case digitCount == 1:
+      return singleDigit(num);
+    case digitCount == 2:
+      return doubleDigits(num);
+    case digitCount == 3:
+      return tripleDigits(num);
+    case digitCount < 7:
+      return thousandsConvert(num);
+    case digitCount < 10:
+      return millionConvert(num)
+  }
 }
 
 module.exports = {
   singleDigit,
   doubleDigits,
   tripleDigits,
-  fourDigits,
-  convertNum
+  thousandsConvert,
+  convertNum,
 };
