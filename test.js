cccbac7a0c5def62d54873d36c45c66385b0a8ca
Cao Mai <caomai@rocketmail.com>
2021-05-12 20:56:29 -0700

    Add Travis CI

--- test.js
+++
@@ -1,6 +0,0 @@
-const converter = require("./index.js")
-
-// console.log(converter.singleDigit(3))
-// console.log(converter.doubleDigits(99))
-// console.log(converter.tripleDigits(999))
-console.log(converter.convertNum(1239345343))
